e79e8f5144e1195aae6d294d81f13277f5a43b45
Nick Wittenberg <nickwittenberg@developersinstitute.co.nz>
2021-09-24 13:11:44 +1200

    Add fetch logic and map over returned book array to display on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [bookList, setBookList] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await fetch("http://localhost:5000/products");
+    const books = await response.json();
+    console.log(books);
+    setBookList(books);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
   return (
     <div className="App">
       <h1>Books and Stuff and a Comb</h1>
       <p>A website for women in their mid 40's who have disposable income</p>
       <ul className="bookList">
-        <li className="bookItem">
-          <h2>The Great Gatsby</h2>
-          <p>F Scott Fitzgerald</p>
-          <p>Genre - tragedy</p>
-          <p>Published - xxxx</p>
-          <img href="someURL" />
-        </li>
+        {bookList.map((book, index) => (
+          <li className="bookItem" key={book.index}>
+            <h2>{book.title}</h2>
+            <p>{book.author}</p>
+            <p>{book.genre}</p>
+            <p>{book.published}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
