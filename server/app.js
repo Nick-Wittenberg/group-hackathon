d2ac3c111a893612faf5b662d6b066a3a5585e69
Kyle <kylevasquez@developersinstitute.co.nz>
2021-09-24 14:43:16 +1200

    fixed model; works now hopefully; organized app.js

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const products = require("./data/data.json");
+const booksModel = require("./model/booksModel");
 
 app.use(cors());
 app.use(express.json());
-
-const products = require("./data/data.json");
 
 // Make GET route for "/" that gets all books/gifts
 // Hard code data in data.json
@@ -16,6 +16,11 @@ app.get("/", (req, res) => {
 
 app.get("/products", (req, res) => {
   return res.send(products).status(200);
+});
+
+app.get("/books", async (req, res) => {
+  const books = await booksModel.find({});
+  return res.send(books).status(200);
 });
 
 module.exports = app;
--- server/model/booksModel.js
+++ server/model/booksModel.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose();
+const { Schema } = mongoose;
 
 const booksSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  published: { type: string, required: true },
+  published: { type: String, required: true },
   genre: { type: String, required: true },
 });
 
