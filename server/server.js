5e054bffd7bc61c264ba11b279423f42146e7b57
Nick Wittenberg <nickwittenberg@developersinstitute.co.nz>
2021-09-24 14:20:49 +1200

    Add docker-compose file and db connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const port = process.env.PORT || 5000;
 const app = require("./app");
 
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/mongo");
+
 app.listen(port, () => {
   console.log(`App server started at http://localhost:${port}`);
 });
